58406d50698903e5ceabb4ff02d7d3e9df2a25f8
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-01-10 12:37:32 +0530

    chat screen nearly 1/4th work done

--- screens/AddChatScreen.tsx
+++ screens/AddChatScreen.tsx
@@ -1,16 +1,41 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { StackParamsList } from "../navigation/types";
+import { Input, Button } from "@rneui/themed";
+import { Ionicons } from "@expo/vector-icons";
 
 type Props = {};
 
-const AddChatScreen = (props: Props) => {
+type NavProps = NativeStackScreenProps<StackParamsList>;
+
+const AddChatScreen = ({ navigation }: NavProps) => {
+  const [inputChat, setInputChat] = React.useState<string>("");
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Add a chat",
+      headerBackTitle: "Chats",
+    });
+  }, [navigation]);
+
+  const createChatHandler = async () => {};
+
   return (
-    <View>
-      <Text>AddChatScreen</Text>
+    <View style={styles.container}>
+      <Input
+        placeholder="Chat name"
+        value={inputChat}
+        onChangeText={(text) => setInputChat(text)}
+        leftIcon={<Ionicons name="chatbubbles" size={24} color="black" />}
+      ></Input>
+      <Button onPress={() => createChatHandler} title="Create chat"></Button>
     </View>
   );
 };
 
 export default AddChatScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+});
